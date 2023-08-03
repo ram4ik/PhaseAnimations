85eec440e0bd37d6e6292ed9c33fc0d1d2510fe9
Ramill <ramill_ibragimov@icloud.com>
2023-08-03 11:45:59 +0300

    init

--- PhaseAnimations/ContentView.swift
+++ PhaseAnimations/ContentView.swift
@@ -8,14 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var animate = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack {
+                Circle()
+                    .fill(.red.gradient)
+                Circle()
+                    .fill(.yellow.gradient)
+                Circle()
+                    .fill(.green.gradient)
+            }
+            .scaleEffect(animate ? 1.0 : 0.5) // inside phaseAnimator
+            .navigationTitle("PhaseAnimation")
+            .onTapGesture {
+                withAnimation {
+                    animate.toggle()
+                }
+            }
+            //.phaseAnimator([1.0, 0.5], trigger: animate) { view, phase in
+            //  view.scaleEffect(phase)
+            //}
         }
-        .padding()
     }
 }
 
